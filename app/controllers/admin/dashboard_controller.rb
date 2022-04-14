16055d006e264941fb42f83e761ab8426d784f7e
David Chan <dchan902@gmail.com>
2022-04-14 17:57:06 -0600

    add password present logic

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["PASSWORD"]
+  http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["PASSWORD"],
+  if: -> { ENV["PASSWORD"].present? }
   def show
   end
 end
