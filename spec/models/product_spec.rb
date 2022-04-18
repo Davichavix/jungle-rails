1c6878c5732492f9009f5d7aaccf0f9167acd4ff
David Chan <dchan902@gmail.com>
2022-04-17 21:55:05 -0600

    add testing for product attributes and error messages

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,5 +12,25 @@ RSpec.describe Product, type: :model do
       it "should create valid product" do
         expect(product).to be_valid
       end
+      it "product should have name" do
+        product.name = nil
+        expect(product).not_to be_valid
+        expect(product.errors.full_messages).to include "Name can't be blank"
+      end
+      it "product should have quantity" do
+        product.quantity = nil
+        expect(product).not_to be_valid
+        expect(product.errors.full_messages).to include "Quantity can't be blank"
+      end
+      it "product should have price" do
+        product.price_cents = nil
+        expect(product).not_to be_valid
+        expect(product.errors.full_messages).to include "Price can't be blank"
+      end
+      it "product should have category" do
+        product.category = nil
+        expect(product).not_to be_valid
+        expect(product.errors.full_messages).to include "Category can't be blank"
+      end
   end
 end
