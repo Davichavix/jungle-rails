f82212d1eab094f092ed5768dcba55bb18e6c816
David Chan <dchan902@gmail.com>
2022-04-19 20:25:01 -0600

    add validation for email uniqueness and unit test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+
+  # def create
+  #   if user = User.authenticate_with_credentials(params[:email], params[:password])
+  #     # success logic, log them in
+  #   else
+  #     # failure, render login form
+  #   end
+  # end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    before(:example) do
+      @user = User.new({
+        name: "David",
+        email: "David@email.com",
+        password: "password",
+        password_confirmation: "password"
+      })
+    end
+    it 'creates a valid new user' do
+      expect(@user).to be_valid
+    end
+
+    it 'user should have name' do
+      @user.name = nil
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Name can't be blank"
+    end
+
+    it 'user should have email' do
+      @user.email = nil
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+
+    it 'user should have password' do
+      @user.password = nil
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+
+    it 'user should have password_confirmation' do
+      @user.password_confirmation = nil
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Password confirmation can't be blank"
+    end
+
+    it 'user password should match password confirmation' do
+      @user.password_confirmation = 'newpassword'
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+
+    it 'user password should match password confirmation' do
+      @user.password_confirmation = 'newpassword'
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+
+    it 'email must be unique' do
+      @user.save
+      @userTwo = User.new({
+        name: "David",
+        email: "DAVID@email.com",
+        password: "password",
+        password_confirmation: "password"
+      })
+      @userTwo.save
+      expect(@userTwo).to_not be_valid
+      expect(@userTwo.errors.full_messages).to include "Email has already been taken"
+    end
+  end
 end
